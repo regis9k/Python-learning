972fe7011c9c92fc2b719d0a475e0b5e64aec107
Oskar Dabrowski <regis9k@gmail.com>
2023-04-01 16:51:59 +0200

    2023.04.01

--- 001_PY4E/crap.py
+++ 001_PY4E/crap.py
@@ -1,25 +1,7 @@
-# def solve(s):
-#     s=s.strip()
-#     s=' '+s
-#     x=0
-#     for i in s:
-#         if i==' ':
-#             char=s[x+1:x+2].strip()
-#             s=s.replace(char,char.capitalize())
-        
-#         x=x+1
-#     return(s.strip())
-# print(solve('oskar dabrowski'))
+t=[1,2,3,4,5,6]
+x='asd'
+# t.append(x)
+# print(t)
 
-
-def solve(s):
-    s=s.strip().capitalize()
-    x=0
-    for i in s:
-        if i==' ':
-            pos=s[x+1].strip()
-            s=s.replace(pos,pos.capitalize())
-        
-        x=x+1
-    return(s.strip())
-print(solve('oskdar dabrowski sdbibiwbdi   asdbwid'))
+t=t+x
+print(t)
